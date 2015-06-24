d0ee7cefa2ada86e022afe816efa4b6766c89f70
Duong <hanhkhatxahoa@gmail.com>
2015-06-24 18:32:08 +0700

    cm

---
+++ BaoCaoDoanhThu.sql
@@ -0,0 +1,12 @@
+﻿alter procedure BaoCaoDoanhThu
+	@Thang int = 1
+	  
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT HIEUXE.TenHieuXe,(select count(*) from HOSOSUACHUA where HIEUXE.MaHX=HOSOSUACHUA.MaHX AND MONTH(HOSOSUACHUA.NgayTiepNhan)=@Thang) as SoLuotSua,(select HOSOSUACHUA.TongCong from HOSOSUACHUA where HIEUXE.MaHX=HOSOSUACHUA.MaHX and MONTH(HOSOSUACHUA.NgayTiepNhan)=@Thang) as DoanhSo,(select count(*) from HOSOSUACHUA where HIEUXE.MaHX=HOSOSUACHUA.MaHX AND MONTH(HOSOSUACHUA.NgayTiepNhan)=@Thang)/CAST((select count(*) from HOSOSUACHUA where MONTH(HOSOSUACHUA.NgayTiepNhan)=@Thang) as float) as  TiLe FROM HIEUXE
+END
